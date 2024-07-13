75fd5ec11d9cc13b6a2a40da424b0f338b837a3b
Elias Meireles <eliasmflilico@gmail.com>
2024-07-13 13:08:56 -0300

    feat: sample authorization request app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [url, setUrl] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [message, setMessage] = useState('');
+    const [response, setResponse] = useState(null);
+    const [success, setSuccess] = useState(false);
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        setResponse(null);
+
+        axios.post(url, {
+            username,
+            password
+        }).then((response) => {
+            console.log(response.status)
+            if (response?.status === 200) {
+                setSuccess(true);
+                setMessage(`Login successful!`);
+                setResponse(JSON.stringify(response.data, null, 2));
+            } else {
+                setResponse(JSON.stringify(response, null, 2));
+                setSuccess(false);
+                setMessage('Login failed!');
+            }
+        }).catch((err) => {
+            setSuccess(false);
+            if (err?.response?.data) {
+                setResponse(JSON.stringify(err.response.data, null, 2));
+            } else {
+                try {
+                    setResponse(JSON.stringify(err, null, 2));
+                } catch (error) {
+                    setResponse(error);
+                }
+            }
+        });
+
+    };
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Login</h1>
+                <form onSubmit={handleLogin}>
+                    <div className={'input-container'}>
+                        <label>Url: </label>
+                        <input
+                            type="text"
+                            value={url}
+                            onChange={(e) => setUrl(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div className={'input-container'}>
+                        <label>Username: </label>
+                        <input
+                            type="text"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div className={'input-container'}>
+                        <label>Password: </label>
+                        <input
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <button type="submit">Login</button>
+                </form>
+            </header>
+            <div className={'content-response'}>
+                <h3 className={success ? 'success' : 'failed'}>{message}</h3>
+
+                <div className={response != null ? "response-container" : 'hidden'}>
+                    <pre className="response-json">{response}</pre>
+                </div>
+
+            </div>
+        </div>
+    );
 }
 
 export default App;
